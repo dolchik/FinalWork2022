32604009dd50a7532bc3ee2e32dff34fad387326
OLGA DRACH <dolchik88@mail.ru>
2022-12-31 16:09:58 +0300

    Добавлена программа итогового задания

---
+++ Code/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] ArrayThreeItems(string[] array) //Метод проверяет элементы входного массива на длину и из подходящих сздает новый массив
+{
+    int arraySizeNew = 0;
+    string[] arrayNew = new string[arraySizeNew];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length == 3)
+        {
+            arraySizeNew++;
+            Array.Resize(ref arrayNew, arraySizeNew);
+            arrayNew[arraySizeNew - 1] = array[i];
+        }
+    }
+    return arrayNew;
+}
+
+Console.Clear();
+string[] array = { "awa1", "tt2", "hgti", "t94", "asdf", "nbngjf" };
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+string[] arrayNew = ArrayThreeItems(array);
+Console.WriteLine("[" + string.Join(", ", arrayNew) + "]");
